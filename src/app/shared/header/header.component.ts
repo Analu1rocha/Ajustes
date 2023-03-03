569d54897051950a11dcea0e8e9222423d10cc0f
Analu Rocha <analua.rocha@outlook.com>
2023-03-03 02:33:33 -0300

    ajustes abas

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -34,13 +34,18 @@ export class HeaderComponent {
 public openPDF(): void {
   let pdf: any = document.querySelector('.htmlData2');
   html2canvas(pdf).then((canvas) => {
-    let fileWidth = 208;
-    let fileHeight = (canvas.height * fileWidth) / canvas.width;
+    let fileWidth = 192;
+    let fileHeight = (canvas.height * fileWidth+50) / canvas.width;
     const FILEURI = canvas.toDataURL('image/png');
+
     let PDF = new jsPDF('p', 'mm', 'a4');
-    let position = 0;
-    PDF.addImage(FILEURI, 'PNG', 0, position, fileWidth, fileHeight);
+    let position = 0 ;
+
+    PDF.addImage(FILEURI, 'PNG', 10, position, fileWidth, fileHeight,);
+
     PDF.save('Tabela.pdf');
+
+
   });
 }
 
